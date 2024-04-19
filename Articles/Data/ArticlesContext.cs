198c028a195268f3806d5a5064822f17f2b2b48a
TkDevk <duvanuriel@hotmail.com>
2024-04-19 13:09:22 -0500

    Add project files.

---
+++ Articles/Data/ArticlesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Articles.Models;
+
+namespace Articles.Data
+{
+    public class ArticlesContext : DbContext
+    {
+        public ArticlesContext (DbContextOptions<ArticlesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Articles.Models.Article> Article { get; set; } = default!;
+    }
+}
---
+++ Articles/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿namespace Articles.Models
+{
+    public class Article
+    {
+        //Properties
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //ctrl+shift+b
+
+    }
+}
